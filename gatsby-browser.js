813e40f783e8ee1e16cb080cbe2f2ab32839b65f
ko Hyunsu <movie3342@gmail.com>
2022-03-23 17:08:17 +0900

    change menu button logic with useState and useEffect

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,17 +1,11 @@
 import React from "react";
 import Layout from "./src/components/Layout";
-<<<<<<< HEAD
 require("prismjs/themes/prism.css");
-=======
->>>>>>> 7fe2f870cb2f07f44f0c2e104c94360a5cef831b
 
 export function wrapPageElement({ element, props }) {
   return <Layout {...props}>{element}</Layout>;
 }
-<<<<<<< HEAD
 
 export function wrapRootElement({ element }) {
   return <>{element}</>;
 }
-=======
->>>>>>> 7fe2f870cb2f07f44f0c2e104c94360a5cef831b
--- src/markdown-pages/react/wired-use-state.md
+++
@@ -1,7 +0,0 @@
----
-slug: "/blog/react/use-hooks"
-date: "2022-03-22"
-title: "으잉? useState, useEffect"
-tags: ["javascript", "react", "hooks", "useState", "useEffect"]
----
-
