d3df24cfb061f44fcbd7fae75030e43f83773047
A <movie3342@gmail.com>
2022-08-30 06:00:34 +0900

    make font.css

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -1,11 +1,7 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-@font-face {
-	font-family: 'SUIT Variable';
-	font-weight: 100 900;
-	src: url('https://cdn.jsdelivr.net/gh/sunn-us/SUIT/fonts/variable/woff2/SUIT-Variable.css') format('woff2-variations');
-}
+
 html,body,h1,h2,h3,h4,h5,h6,p{
     margin:0;
     padding:0;
