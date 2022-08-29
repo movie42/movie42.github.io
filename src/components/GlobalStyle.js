a8a15aa141d59c4670d81ddaab15ec04a4659196
movie42 <movie3342@gmail.com>
2022-08-30 05:20:24 +0900

    change font

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -1,25 +1,25 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-
+@font-face {
+	font-family: 'SUIT Variable';
+	font-weight: 100 900;
+	src: url('https://cdn.jsdelivr.net/gh/sunn-us/SUIT/fonts/variable/woff2/SUIT-Variable.css') format('woff2-variations');
+}
 html,body,h1,h2,h3,h4,h5,h6,p{
     margin:0;
     padding:0;
 }
 
 html,body,form,input,textarea,label,button {
-    font-size:10px;
-    font-family: 'Noto Sans KR', sans-serif;
+    font-size: 10px;
+    font-family: 'SUIT-Medium', sans-serif;
 }
-
-body {    
-    font-size:1.6rem;
-    color:${props => props.theme.basicColor}
+p{
+    font-size:1.85rem;
 }
-
-
-
 ul, ol, dl, li {
+    font-size:1.85rem;
     list-style: none;
 }
 
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -3,15 +3,8 @@ import Typography from "typography";
 const typography = new Typography({
   baseFontSize: "10px",
   baseLineHeight: 1.666,
-  googleFonts: [
-    { name: "Noto Sans KR", styles: [100, 500, 900] },
-    {
-      name: "JetBrains Mono",
-      styles: [200],
-    },
-  ],
-  headerFontFamily: ["Noto Sans KR", "sans-serif"],
-  bodyFontFamily: ["Noto Sans KR", "sans-serif"],
+  headerFontFamily: ["SUIT Variable", "sans-serif"],
+  bodyFontFamily: ["SUIT Variable", "sans-serif"],
 });
 
 export default typography;
